071b44752f78330e55010df0ccafe786cba5d695
Mitin Sharma <mitinsharma@Mitins-M1-Pro.fios-router.home>
2022-09-07 16:01:44 -0400

    responsive hamburger menu added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 console.log('yay')
 
-const hamburger = document.querySelector("#hamburger")
-const menu = document.querySelector("#menu")
+var hamburger = document.querySelector("#hamburger");
+var menu = document.querySelector("#menu");
 
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("hidden")
+  hamburger.classList.toggle("bg-white")
 });
