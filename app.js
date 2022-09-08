201617f4e04d479ee99c357fe6881a01119e815f
Mitin Sharma <mitinsharma@Mitins-M1-Pro.fios-router.home>
2022-09-07 20:08:00 -0400

    Links added

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-console.log('yay')
-
-var hamburger = document.querySelector("#hamburger");
-var menu = document.querySelector("#menu");
-
+var hamburger = document.querySelector("#hamburger")
+var menu = document.querySelector("#menu")
+var hLinks = document.querySelectorAll("#hLink")
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("hidden")
   hamburger.classList.toggle("bg-white")
+});
+
+hLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.toggle("hidden")
+    hamburger.classList.toggle("bg-white")
+  });
 });
